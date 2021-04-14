d178d03c465eee21717dfa908238753cce5c88eb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-14 10:45:22 +0000

    Deploy Arlabunakty/hello_flutter_web_application to github.com/Arlabunakty/hello_flutter_web_application.git:gh-pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "7f7b2c9607172956edb447966e5a5d10",
-"/": "7f7b2c9607172956edb447966e5a5d10",
+  "index.html": "c4b6573a4f31a6c07896a81fc0c50e70",
+"/": "c4b6573a4f31a6c07896a81fc0c50e70",
 "main.dart.js": "0d56dc3759c57c9ca4bb3f73bb52e52b",
 "version.json": "e080dff72089cf83502f62acbaca3846",
 "assets/FontManifest.json": "d751713988987e9331980363e24189ce",
